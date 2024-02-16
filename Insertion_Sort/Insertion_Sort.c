1cb7a3601f42f01978dd7c53dd87aed0bac830d4
Ahmed Ez-Zouine <azouine@gmail.com>
2024-02-16 11:07:08 +0100

    done

--- Insertion_Sort/Insertion_Sort.c
+++ Insertion_Sort/Insertion_Sort.c
@@ -7,3 +7,24 @@ void ft_swap(int *a, int *b)
   *a = *b;
   *b = temp;
 }
+
+void Insertion_Sorting(int array[], int count)
+{
+  int i;
+  int j;
+
+  int value;
+  i = 1;
+  while (i < count)
+  {
+    value = array[i];
+    j = i + 1;
+    while (value < array[j] && j >= 0)
+    {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = value;
+  }
+  i++;
+}
